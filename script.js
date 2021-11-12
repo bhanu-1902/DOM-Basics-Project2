1a304cec96367730794c84a4b8602f8d86c2c9b1
Harshit Bhanushali <harshit.21810372@viit.ac.in>
2021-11-12 11:34:52 +0530

    Complete adding, removing overlays by buttons

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+const showModal = document.querySelectorAll('.show-modal'); //whenever there are multiple elements with the same class, querySelector selects only the first one and hence we have used querySelectorAll here
+
+// for (let i = 0; i < showModal.length; i++) {
+//   showModal[i].addEventListener('click', function () {
+//     console.log('clicked');
+//   });
+// }
+
+const addHidden = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const removeHidden = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+//Conventionally used to iterate over iterable objects
+for (let i of showModal) {
+  i.addEventListener('click', removeHidden);
+}
+
+closeModal.addEventListener('click', addHidden);
+
+overlay.addEventListener('click', addHidden);
